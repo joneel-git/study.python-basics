c7df58d01059d6e1c0bc0ebfa406906749f71602
joneel-git <johnj.es@outlook.com>
2024-07-23 14:03:51 +0200

    ternary-operator

---
+++ basic-02/hello.py
@@ -0,0 +1,15 @@
+# https://www.youtube.com/watch?v=H2EJuAcrZYU 27:16
+    
+# Example if else vs Ternary operator 
+# wheather = "It's Raining"
+
+# if wheather == "sunny":
+#     print("Let's go outside") # True
+# else:
+#     print('Not today!.. ') # False
+
+# Ternary operator
+# Works little different than other languages we begin by writing the true statemnt first what we want.
+wheather = "sunny"
+print("Let's go outside!.") if wheather == "sunny" else print("Not Today...!") # Output True it's sunny.
+    #   True                                         #   False
