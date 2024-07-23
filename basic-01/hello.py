714b2b77be4a79c1b62fb380178f5f8af38e4932
joneel-git <johnj.es@outlook.com>
2024-07-23 09:51:23 +0200

    keywords: And, Or and Not

---
+++ basic-01/hello.py
@@ -0,0 +1,15 @@
+# https://www.youtube.com/watch?v=H2EJuAcrZYU 3.39
+
+
+
+x = True ###
+y = False
+z = True
+
+# Here we are saying y is NOT true
+print(not y)
+
+
+# The and keywoard is looking for second value ONLY if the FIRST value is true
+
+print(y and z)
