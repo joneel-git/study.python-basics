0efda9342ea69e56670b34a652d7de6bcb8f50c5
joneel-git <johnj.es@outlook.com>
2024-07-23 14:45:56 +0200

    data_types

---
+++ basic-03/data_types.py
@@ -0,0 +1,13 @@
+# https://www.youtube.com/watch?v=H2EJuAcrZYU 43:19
+# Lesson 4
+# String Data type
+
+# Literal assignment 
+first = "John"
+last = " Doe"
+print("")
+print(type(first))
+
+
+print("\n", type(first) == str) # Is this realy a string literal?
+print(isinstance(first, str)) # Is this an instance of a str class? 
