ae1ac17aa7c85dc271aab3741a248850ce6eb783
joneel-git <johnj.es@outlook.com>
2024-07-26 09:50:36 +0200

    data_types

--- basic-03/data_types.py
+++ basic-03/data_types.py
@@ -6,12 +6,10 @@
 age = 21  # Example: Your age
 players = 2  # Example: Number of players online
 quantity = 5  # Example: Quantity of items to buy
-print("--")
+
 print(f"You are {age} years old")
 print(f"There are {players} players online")
 print(f"You would like to buy {quantity} items")
-
-print("--")
 
 # FLOAT
 # A float is a number that includes a decimal point
@@ -19,37 +17,38 @@ gpa = 3.2  # Example: Grade point average
 distance = 2.5  # Example: Distance ran in kilometers
 price = 10.99  # Example: Price in dollars
 
-print(f"Your GPA is {gpa}")
+print(f"\nYour GPA is {gpa}")
 print(f"You ran {distance} Km")
 print(f"The price is ${price}")
 
-print("--")
+
 # STRING
 # A string is a sequence of characters, like words or sentences
 name = "dude bro"  # Example: A person's name
 food = "Pizza"  # Example: A favorite food
 email = "random@email.com"  # Example: An email address
 
-print(f"Your name is {name}")
+print(f"\nYour name is {name}")
 print(f"My favorite food is {food}")
 print(f"My email is {email}")
 
-print("--")
+
 # BOOLEAN
 # A boolean is like a light switch, it can be either on (True) or off (False)
 # It represents whether something is happening or not
+
 online = True  # Example: Whether you are online
 for_sale = True  # Example: Whether an item is for sale
 running = False  # Example: Whether a process is running
-print(f"Are you online?: {online}")
+
+print(f"\nAre you online?: {online}")
 print(f"Is this item for sale?: {for_sale}")
 print(f"Is the game running?: {running}")
 
-print("--")
 # IF STATEMENT
 # An if statement checks a condition and decides what to do next based on whether the condition is True or False
 # It's like asking a question: "Is the game running?"
 if running:
-    print("The game is running")
+    print("\nThe game is running")
 else:
-    print("The game is over..")
+    print("\nThe game is over..")
